cbe5157600a3576a63bdb5684ccafa4490025788
Cihan Sari <cihansari86@gmail.com>
2018-08-23 21:14:00 +0200

    Added base entry point for cihansari.com

--- cihansaricom_server/dist/routes/index.js
+++ cihansaricom_server/dist/routes/index.js
@@ -4,23 +4,17 @@ const express = require("express");
 const path = require("path");
 function getRouter() {
     const router = express.Router();
-    if (false) {
-        /* GET home page. */
-        const pathToServe = path.join(__dirname, "../../../cihansaricom_gui/dist");
-        router.use(express.static(pathToServe));
-        router.use((req, res, next) => {
-            // respond with html page
-            if (req.accepts("html") && req.url.indexOf(".") !== -1) {
-                res.sendFile(path.join(pathToServe, "index.html"));
-                return;
-            }
-            next();
-        });
-    }
-    else {
-        // Highjack to redirect until our greeting page is up
-        router.use((req, res, next) => res.redirect(301, "//clothingweb.cihansari.com/"));
-    }
+    /* GET home page. */
+    const pathToServe = path.join(__dirname, "../../../cihansaricom_gui/dist");
+    router.use(express.static(pathToServe));
+    router.use((req, res, next) => {
+        // respond with html page
+        if (req.accepts("html") && req.url.indexOf(".") !== -1) {
+            res.sendFile(path.join(pathToServe, "index.html"));
+            return;
+        }
+        next();
+    });
     return router;
 }
 exports.getRouter = getRouter;
--- cihansaricom_server/lib/routes/index.ts
+++ cihansaricom_server/lib/routes/index.ts
@@ -2,23 +2,16 @@ import * as express from "express";
 import * as path from "path";
 export function getRouter() {
   const router = express.Router();
-  if (false) {
-    /* GET home page. */
-    const pathToServe = path.join(__dirname, "../../../cihansaricom_gui/dist");
-    router.use(express.static(pathToServe));
-    router.use((req, res, next) => {
-      // respond with html page
-      if (req.accepts("html") && req.url.indexOf(".") !== -1) {
-        res.sendFile(path.join(pathToServe, "index.html"));
-        return;
-      }
-      next();
-    });
-  } else {
-    // Highjack to redirect until our greeting page is up
-    router.use((req, res, next) =>
-      res.redirect(301, "//clothingweb.cihansari.com/")
-    );
-  }
+  /* GET home page. */
+  const pathToServe = path.join(__dirname, "../../../cihansaricom_gui/dist");
+  router.use(express.static(pathToServe));
+  router.use((req, res, next) => {
+    // respond with html page
+    if (req.accepts("html") && req.url.indexOf(".") !== -1) {
+      res.sendFile(path.join(pathToServe, "index.html"));
+      return;
+    }
+    next();
+  });
   return router;
 }
